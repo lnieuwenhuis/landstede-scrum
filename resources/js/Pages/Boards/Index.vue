d1df293af77ae046799976b9a94befbba090ccac
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2025-01-27 11:39:55 +0100

    Improved controllers and provided examples of object returning

---
+++ resources/js/Pages/Boards/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+
+const { props } = usePage();
+
+const boards = props.boards;
+</script>
+
+<template>
+    <Head title="Your Boards" />
+
+    <AuthenticatedLayout>
+        <div>
+            <h1>Boards Index</h1>
+            <ul>
+                <li v-for="board in boards" :key="board.id">
+                    <a style="color: blue;" :href="`/boards/${board.id}`">{{ board.title }}</a>
+                </li>
+            </ul>
+            <!-- Display a message if groups is empty -->
+            <p v-if="boards.length === 0">No groups found.</p>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Boards/Show.vue
@@ -0,0 +1,45 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+
+const { props } = usePage();
+
+const board = props.board;
+const columns = props.columns;
+const users = props.users;
+
+console.log(columns);
+</script>
+
+
+<template>
+    <Head title="Board" />
+
+    <AuthenticatedLayout>
+        <div>
+            <h1>{{ board.title }}</h1>
+            <p>{{ board.description }}</p>
+            <br>
+
+            <h2>Members</h2>
+            <ul>
+                <li v-for="user in users" :key="user.id">
+                    {{ user.name }}
+                </li>
+            </ul>
+            <br>
+
+            <h1>Columns</h1>
+            <ul>
+                <li v-for="column in columns" :key="column.id">
+                    <h3>{{ column.title }}</h3>
+                    <ul>
+                        <li v-for="card in column.cards" :key="card.id">
+                            {{ card.title }}
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </AuthenticatedLayout>
+</template>
