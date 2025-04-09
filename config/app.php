b326b435c48a81787a7bfc8832847692b15dd4fb
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2025-04-09 13:09:58 +0200

    mmm

--- config/app.php
+++ config/app.php
@@ -123,4 +123,9 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'providers' => [
+        // Other providers...
+        SocialiteProviders\Manager\ServiceProvider::class,
+    ],
+
 ];
