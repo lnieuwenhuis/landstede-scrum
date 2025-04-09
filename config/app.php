c12fbffc12b46ee4e68eb790dd5e4a04a366a464
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2025-04-09 13:18:47 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -123,9 +123,4 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
-    'providers' => [
-        // Other providers...
-        SocialiteProviders\Manager\ServiceProvider::class,
-    ],
-
 ];
