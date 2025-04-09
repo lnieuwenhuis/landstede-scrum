55695d3f6f11a5d77072057a22cc86a35c6254d1
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2025-04-09 14:09:27 +0200

    optimizefix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,30 +2,18 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
-            // ... other providers
             \SocialiteProviders\Azure\AzureExtendSocialite::class.'@handle',
         ],
     ];
 
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
-        //
+        parent::boot();
     }
 }
