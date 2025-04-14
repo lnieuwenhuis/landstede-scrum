e384bcc8917bb1ecc61245b7523bb60749dd12d1
Jarno4359 <jarno.groenewoud74@gmail.com>
2025-04-14 13:37:22 +0200

    Update UsersTab.vue

--- resources/js/Pages/Boards/ShowComponents/UsersTab.vue
+++ resources/js/Pages/Boards/ShowComponents/UsersTab.vue
@@ -181,7 +181,7 @@ const handleDeleteUser = async (userId) => {
                 <h2 class="text-2xl font-semibold text-gray-800 m-2">Team Members</h2>
             <button 
                 @click="toggleAddUserModal"
-                class="px-3 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center gap-2 mr-2"
+                class="px-3 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center gap-2 mr-2 max-h-12 min-w-max"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
