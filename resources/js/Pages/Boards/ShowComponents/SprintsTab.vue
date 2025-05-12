17867a78b43bd0872d12208cdab42ed86b10dcaf
Jarno4359 <jarno.groenewoud74@gmail.com>
2025-05-12 12:38:01 +0200

    user tab responsive

--- resources/js/Pages/Boards/ShowComponents/SprintsTab.vue
+++ resources/js/Pages/Boards/ShowComponents/SprintsTab.vue
@@ -253,7 +253,7 @@ const emit = defineEmits(['sprint-deleted', 'sprint-updated']);
                     <h2 class="text-2xl font-semibold text-gray-800 m-2">Sprints</h2>
                     <button 
                         @click="handleCreateSprint"
-                        class="px-3 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center gap-2"
+                        class="px-3 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center gap-2 h-10 w-[135px]"
                         :disabled="loading"
                         :class="{ 'opacity-50 cursor-not-allowed': loading }"
                     >
