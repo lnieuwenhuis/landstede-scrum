3fd59903967d19100e8ae003b287f70ab95e3440
Jarno4359 <jarno.groenewoud74@gmail.com>
2025-06-11 13:40:53 +0200

    card description

--- resources/js/Pages/Boards/ShowComponents/CardForm.vue
+++ resources/js/Pages/Boards/ShowComponents/CardForm.vue
@@ -113,6 +113,7 @@ const handleCancel = () => {
                     class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Card description"
                     rows="3"
+                    style="resize: none"
                 ></textarea>
             </div>
             
